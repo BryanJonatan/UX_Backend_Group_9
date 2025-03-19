f76f1c760fbc05a5c2e6229c0ec0f99abb59ecf2
ihsanfarabi <34181167+ihsanfarabi@users.noreply.github.com>
2025-03-19 22:27:07 +0700

    Update models and handlers, add migrations and database files

--- Models/ForumPost.cs
+++ Models/ForumPost.cs
@@ -1,11 +1,14 @@
-﻿namespace PetPals_BackEnd_Group_9.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetPals_BackEnd_Group_9.Models
 {
     public class ForumPost
     {
+        [Key]
         public int ForumPostId { get; set; }
         public int UserId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
-        public object User { get; internal set; }
+        public User User { get; set; } = null!;
     }
 }
--- Models/ServiceCategory.cs
+++ Models/ServiceCategory.cs
@@ -1,7 +1,10 @@
-﻿namespace PetPals_BackEnd_Group_9.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetPals_BackEnd_Group_9.Models
 {
     public class ServiceCategory
     {
+        [Key]
         public int CategoryId { get; set; }
         public string Name { get; set; } = string.Empty;
     }
